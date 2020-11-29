9628381c502ba5ccd321115102ace6c4d0725f08
Howard <hmontesdeoca@github.com>
2020-11-29 14:09:37 -0800

    further refined client

--- client.sh
+++ client.sh
@@ -2,6 +2,8 @@
 
 echo "Welcome to our penguin chat!"
 echo "----------------------------"
+
+#if new user
 if [[ ! -e login.txt ]]; then
     echo "Create a user login"
     read -p "username: " username
@@ -10,6 +12,7 @@ if [[ ! -e login.txt ]]; then
     echo "$password" >> login.txt
     echo
 else
+#if old user verify
     read -p "username: " username
     read -sp "password: " password
     if [[ $username != $(head -1 login.txt) ||  $password != $(tail -1 login.txt) ]]; then
@@ -20,24 +23,37 @@ else
     echo
 fi
 
+#do-while loop
 until [[ $VARNAME == "q" ]] ; do  
     
     read -r VARNAME
-
+    
+    #if it begins with message 
     if [[ $VARNAME =~ ^message\/ ]] ; then
         reciever=$(echo "$VARNAME" | cut -d '/' -f2 )
         
+        #if no user is specified
         if [[ -z "$reciever"  ]]; then
             echo "please specify user to message to"
+            continue
         fi
-
+        
+        #message body to be sent
         message=$(echo "$VARNAME" | cut -d ' ' -f2)
-        curl -H "sender: $username" -H "sender-password: $password" -d "$message" -X POST LOCALHOST:3000/mailbox
+
+        #curl command invoked to send message
+        curl -H "sender: $username" -H "sender-password: $password" -d "$message" -X POST LOCALHOST:3000/message/$reciever
         echo
+
+    #if it is a mailbox command 
     elif [[ $VARNAME == "mailbox/" ]] ; then
         echo "youve got mail!"
+
+        #curl command to recieve messages
+        curl -H "sender: $username" -H "sender-password: $password" -X GET LOCALHOST:3000/mailbox
         echo
-        
+
+    #if it is the options command 
     elif [[ $VARNAME == "/" ]] ; then
         echo "commands"
         echo "-----------------------"
