38a809b59a54a835ec8f79b2c8b96ce785bf22fa
jasonling1 <61610371+jasonling1@users.noreply.github.com>
2020-11-29 17:02:58 -0800

    Add files via upload

--- src/process_message.cpp
+++ src/process_message.cpp
@@ -1,15 +1,23 @@
 #include "process_message.h"
 
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
+
+struct message_t {
+    std::string sender;
+    std::string body;
+};
 
 struct user_data_t {
     std::string password;
     std::vector<message_t> mailbox;
 };
 
-std::string process_message(const std::string packet &) {
+static std::map<std::string, user_data_t> storage;
+
+std::string process_message(const std::string & packet) {
 
     static constexpr auto sender_length = 8;
     static constexpr auto pass_length = 10;
@@ -19,13 +27,45 @@ std::string process_message(const std::string packet &) {
     unsigned second_delimit = request.find_first_of("\r\n", delimit_send);
     unsigned delimit_pass = request.find("password");
     unsigned fourth_delimit = request.find_first_of("\r\n", delimit_pass);
+    unsigned body_delimit = request.find("\r\n\r\n");
 
     std::string username = request.substr(delimit_send + sender_length,
                                           second_delimit - delimit_send - sender_length);
     std::string password
         = request.substr(delimit_pass + pass_length, fourth_delimit - delimit_pass - pass_length);
 
-    std::cout << username << '\n' << password << std::endl;
+    std::string body = request.substr(body_delimit);
 
-    return '';
+    message_t message;
+    message.sender = username;
+    message.body = body;
+    user_data_t user;
+    user.password = password;
+    user.mailbox.push_back(message);
+
+    std::map<std::string, user_data_t>::iterator it = storage.find(username);
+    if (it != storage.end()) {
+        it->second = user;
+    } else
+        storage.insert({username, user});
+
+    return message.body;
+}
+
+std::string get(const std::string & user, const std::string & password) {
+
+    user_data_t returnvalue;
+
+    std::map<std::string, user_data_t>::iterator it = storage.find(user);
+    if (it != storage.end()) {
+        returnvalue = storage.at(user);
+        if (password == returnvalue.password) {
+            for (int i = 0; i < returnvalue.mailbox.size(); ++i) {
+                std::cout << returnvalue.mailbox[i].sender << ": " << returnvalue.mailbox[i].body;
+            }
+            return "End of message.";
+        }
+    }
+
+    return "badinput";
 }
